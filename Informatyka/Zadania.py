849eeba35f875c490a85903115ade11326f0d507
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2024-09-04 14:09:40 +0100

    Create Zadania.py

---
+++ Informatyka/Zadania.py
@@ -0,0 +1,44 @@
+\\Adres strony z zadaniami: tiny.pl/j08n-qy6
+
+#def czy_podielna_przez_2(n):
+#    return n % 2 == 0
+
+#Zad.1
+# n = int(input("Podaj liczbę: "))
+# if czy_podielna_przez_2(n):
+#     print("Tak")
+# else:
+#     print("Nie")
+
+#Zad.2
+#a) złożóność - n
+m = int(input("Podaj Liczbę: "))
+# for i in range(1, m+1):
+#     if m % i == 0:
+#         print(i)
+#b)złożóność - pierwiastek z n
+# i = 1
+# while i*i<n:
+#     if n%i==0:
+#         print(i)
+#         print(int(n/i))
+#     i=i+1
+# if i*i==n:
+#     print(i)
+
+#Zad.3
+# l = int(input("Podaj Liczbę: "))
+# suma = 0
+# for i in range(1, l+1):
+#     if l % i == 0:
+#         suma += i
+# print(suma)
+
+#Zad.4
+# p = int(input("Podaj Liczbę: "))
+# ilosc_dzielnikow = 0
+# for i in range(1, p+1):
+#     if p % i == 0:
+#         ilosc_dzielnikow += 1
+# print(ilosc_dzielnikow)
+
