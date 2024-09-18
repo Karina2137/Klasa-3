47f737858b93a3145af6d0bd028c2f813a17269e
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2024-09-18 15:05:31 +0200

    Update Zadania.py

--- Informatyka/Pliki/Zadania.py
+++ Informatyka/Pliki/Zadania.py
@@ -7,13 +7,57 @@ def czy_pierwsza(n):
         if n%i==0:
             return False
     return True
-
+    
+#Zad.1
 plik = open("liczby1.txt","r") #otwieranie pliku
 liczby = list(map(int,plik.read().split())) #tworzenie listy z zawartości pliku
-#Zad.1
 ile = 0
 for liczba in liczby:
     if czy_pierwsza(liczba):
         ile += 1
 print(ile)
 plik.close() #zamknięcie pliku
+
+#Zad.2
+plik = open("liczby2.txt","r")
+liczby = list(map(int,plik.read().split()))
+for liczba  in liczby:
+    if liczba % 2 == 0:
+        print(liczba, end="\n")
+plik.close()
+
+#Zad.3
+def liczba_dzielnikow_wlasciwych(n):
+    ile = 0
+    for i in range(1,n):
+        if n % i == 0:
+            ile += 1
+    return ile
+plik = open("liczby2.txt","r")
+liczby = list(map(int,plik.read().split()))
+plik.close()
+suma = 0
+for liczba in liczby:
+    suma+=liczba_dzielnikow_wlasciwych(liczba)
+print(suma)
+
+#Zad.5
+def suma_czynnikow_pierwszych(n):
+    czynnik = 2
+    suma = 0
+    while n>1:
+        while n % czynnik == 0:
+            suma += czynnik
+            n //= czynnik
+        czynnik += 1
+    return suma
+plik = open("liczby2.txt","r")
+liczby = list(map(int,plik.read().split()))
+plik.close()
+suma_czynnikow = []
+for liczba in liczby:
+    suma_czynnikow.append(suma_czynnikow_pierwszych(liczba))
+minimum = min(suma_czynnikow)
+for i in range(len(liczby)):
+    if suma_czynnikow[i] == minimum:
+        print(liczby[i])
