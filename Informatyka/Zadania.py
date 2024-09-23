469099697580055948f06cc86d35e73ac8e0b247
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2024-09-23 20:10:09 +0200

    Update Zadania.py

--- Informatyka/Zadania.py
+++ Informatyka/Zadania.py
@@ -1,21 +1,17 @@
 #Adres strony z zadaniami: tiny.pl/j08n-qy6
 
-#def czy_podielna_przez_2(n):
-#    return n % 2 == 0
-
-#Zad.1
-# n = int(input("Podaj liczbę: "))
-# if czy_podielna_przez_2(n):
-#     print("Tak")
-# else:
-#     print("Nie")
-
-#Zad.2
-#a) złożóność - n
-m = int(input("Podaj Liczbę: "))
-# for i in range(1, m+1):
-#     if m % i == 0:
-#         print(i)
+# #zad.1
+a = int(input())
+if a%2==0:
+    print("tak")
+else:
+    print("nie")
+# #zad.2
+b = int(input())
+for i in range(1,b+1):
+    if b%i==0:
+        print(i, end=" ")
+print()
 #b)złożóność - pierwiastek z n
 # i = 1
 # while i*i<n:
@@ -25,86 +21,93 @@ m = int(input("Podaj Liczbę: "))
 #     i=i+1
 # if i*i==n:
 #     print(i)
-
-#Zad.3
-# l = int(input("Podaj Liczbę: "))
-# suma = 0
-# for i in range(1, l+1):
-#     if l % i == 0:
-#         suma += i
-# print(suma)
-
-#Zad.4
-# p = int(input("Podaj Liczbę: "))
-# ilosc_dzielnikow = 0
-# for i in range(1, p+1):
-#     if p % i == 0:
-#         ilosc_dzielnikow += 1
-# print(ilosc_dzielnikow)
-
-#Zad.5
-import math
-
+# #zad.3
+c = int(input())
+suma = 0
+for i in range(1,c+1):
+    if c%i==0:
+        suma += i
+print(suma)
+#zad.5
 def czy_pierwsza(n):
     if n<2:
         return False
     for i in range(2,n):
-        if n%i==0: return False
+        if n%i==0:
+            return False
     return True
 
-# n = int(input("Podaj n: "))
-# for i in range(2, n+1):
-#     if n % i == 0 and czy_pierwsza(i):
-#         print(i)
+d = int(input())
+for i in range(2,d):
+    if d%i==0 and czy_pierwsza(i):
+        print(i, end=" ")
 
-#Zad.6
-# a = int(input("Podaj a: "))
-# b = int(input("Podaj b: "))
-# if czy_pierwsza(a) and czy_pierwsza(b) and (math.ceil(a-b)==2 or math.ceil(b-a)==2): print(f"Liczby {a} i {b} są bliźniacze")
-# else: print(f"Liczby {a} i {b} nie są bliźniacze")
+#zad.6
+def czy_pierwsza(n):
+    if n<2:
+        return False
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    return True
 
-#Zad.7
-# n = int(input("Podaj liczbę: "))
-# x = 0
-# y = 0
-# while (n - 1 != x):
-#     x = x + 1
-#     if (n % x == 0):
-#         y = y + x
-# if (y == n):
-#     print("Tak")
-# else:
-#     print("Nie")
-    
-#Zad.8
-# p = int(input("Podaj liczbę: "))
-# d = int(input("Podaj liczbę: "))
-# def sumaDzielnikow(n):
-#     x = 0
-#     y = 0
-#     while (n - 1 != x):
-#         x = x + 1
-#         if (n % x == 0):
-#             y = y + x
-#     return y
-# if (sumaDzielnikow(p) == d and sumaDzielnikow(d) == p):
-#     print("Tak")
-# else:
-#     print("Nie")
+e = int(input())
+f = int(input())
+if czy_pierwsza(e) and czy_pierwsza(f):
+    if e-f==2 or f-e==2:
+        print("tak")
+    else:
+        print("nie")
+else:
+    print("nie")
 
-#Zad.9
-# def czyLiczbyBlizniacze(p,d):
-#     def czyDzielnikPierwszy(n):
-#         x = 0
-#         y = -1
-#         while (n != x):
-#             x = x + 1
-#             if (n % x == 0):
-#               y = y + 1
-#         if (y == 1):
-#             return True
-#         else:
-#             return False
-#     if (czyDzielnikPierwszy(p) == True and czyDzielnikPierwszy(d) == True):
-#         if (p - d == 2 or p - d == 2):
-#             print(p and d)
+#zad.7
+g = int(input())
+suma=0
+for i in range(1,g):
+    if g%i==0:
+        suma+=i
+if suma == g:
+    print("tak")
+else:
+    print("nie")
+
+#zad.8
+h = int(input())
+j = int(input())
+suma_h = 0
+suma_j = 0
+for i in range(1,h):
+    if h%i==0:
+        suma_h += i
+for i in range(1,j):
+    if j%i==0:
+        suma_j += i
+if suma_h == j and suma_j == h and j!=h:
+    print("tak")
+else:
+    print("nie")
+
+#zad.9
+def czy_pierwsza(n):
+    if n<2:
+        return False
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    return True
+
+for i in range(1,1001):
+    if czy_pierwsza(i) and czy_pierwsza(i+2):
+        print(i,i+2)
+
+#zad.10
+def suma_dzielnikow_wlasciwych(n):
+    suma = 0
+    for i in range(1,n):
+        if n%i==0:
+            suma+=i
+    return suma
+for i in range(1,1001):
+    if suma_dzielnikow_wlasciwych(i) == i:
+        print(i, end=" ")
