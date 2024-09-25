c405686f8784ea27ef6b3dd5205b1d3e3ecd37f0
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2024-09-25 15:05:30 +0200

    Update Sito eratostenesa.py

--- Informatyka/Sito eratostenesa.py
+++ Informatyka/Sito eratostenesa.py
@@ -1,1 +1,15 @@
-
+#zad.1
+#Sito Erotostenesa - algortm podstawowy
+n = int(input())
+T = [True] * (n+1)
+T[0] = False
+T[1] = False
+p = 2
+while p*p <= n:
+    if T[p]:
+        for i in range(p*p,n+1,p):
+            T[i] = False
+    p += 1
+for i in range(2,n+1):
+    if T[i] == True:
+        print(i, end=" ")
