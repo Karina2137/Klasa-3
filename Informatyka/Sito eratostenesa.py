c75af1793efc3527ef7d7f1d6b1b923542f9d486
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2024-10-01 11:11:18 +0200

    Update Sito eratostenesa.py

--- Informatyka/Sito eratostenesa.py
+++ Informatyka/Sito eratostenesa.py
@@ -13,3 +13,40 @@ while p*p <= n:
 for i in range(2,n+1):
     if T[i] == True:
         print(i, end=" ")
+
+#zad.2
+n = int(input())
+T = [True] * (n+1)
+T[0] = False
+T[1] = False
+p = 2
+liczba = 0
+suma = 0
+while p*p <= n:
+    if T[p]:
+        for i in range(p*p,n+1,p):
+            T[i] = False
+    p += 1
+for i in range(2,n+1):
+    if T[i] == True:
+        suma += i
+        liczba += 1
+print(suma," ", liczba, end=" ")
+
+#zad.3
+n = int(input())
+T = [True] * (n+1)
+T[0] = False
+T[1] = False
+p = 2
+while p*p <= n:
+    if T[p]:
+        i=p*p
+        while i<n+1:
+            T[i] = False
+            i+=p
+    p += 1
+for i in range(2,n+1):
+    if T[i] == True:
+        print(i, end=" ")
+
