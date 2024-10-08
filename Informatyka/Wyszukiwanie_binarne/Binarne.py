2fec4e2d62e055b80e1f44f919178a2fd6fa016b
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2024-10-08 11:08:47 +0200

    Update Binarne.py

--- Informatyka/Wyszukiwanie_binarne/Binarne.py
+++ Informatyka/Wyszukiwanie_binarne/Binarne.py
@@ -1,9 +1,10 @@
 def czy_nie_malejaca(T,n):
     for i in range(n-1):
-        if not T[i] > T[i+1]:
+        if T[i] > T[i+1]:
             return False
-        return True
+    return True
 
+#Zad.1
 def wyszukiwanie_binarne(T, a, n):
     lewy = 0
     prawy = n-1
@@ -15,20 +16,26 @@ def wyszukiwanie_binarne(T, a, n):
             prawy = srodek
         if T[lewy]==a:
             return T[lewy]
-T = [1,2,3,4,5,6,7,8,9,10]
-print(wyszukiwanie_binarne(T,4,10))
-print()
-
-#Zad.1
-L = []
+            
+L = list(map(int, input().split()))
 l = int(input())
-for i in range(10):
-    n = int(input())
-    L.append(n)
-
-if czy_nie_malejaca(T,T(len)):
+if czy_nie_malejaca(L,L(len)):
     print(wyszukiwanie_binarne(L,l,len[L]))
 else:
     print("Nie")
 
 #Zad.2
+def wyszukiwanie_binarne_rek(T, a, lewy, prawy):
+    if lewy < prawy:
+        srodek = (lewy+prawy)//2
+        if T[srodek] < a:
+            return wyszukiwanie_binarne_rek(T, a, srodek+1, prawy)
+        else:
+            return wyszukiwanie_binarne_rek(T, a, lewy, srodek)
+    return T[lewy] == a
+T = list(map(int, input().split()))
+a = int(input())
+if czy_nie_malejaca(T,len(T)):
+    print(wyszukiwanie_binarne_rek(T, a, 0, len(T)-1))
+else:
+    print("Ciąd musi być uporządkowany")
