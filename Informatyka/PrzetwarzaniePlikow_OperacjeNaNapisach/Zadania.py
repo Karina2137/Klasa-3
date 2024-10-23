8e674b0c9e9388023adadf92bff18a04d27a9bce
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2024-10-23 15:08:37 +0200

    Update Zadania.py

--- Informatyka/PrzetwarzaniePlikow_OperacjeNaNapisach/Zadania.py
+++ Informatyka/PrzetwarzaniePlikow_OperacjeNaNapisach/Zadania.py
@@ -24,3 +24,31 @@ for linia in plik:
     if ilosc % 2==0:
         suma+=1
 print(suma)
+
+# zad.5
+print("a. wyświetli ponumerowane słowa z pliku tekstowego")
+plik = open('slowa.txt','r')
+slowa = list(map(str,plik.readlines()))
+for i in range(len(slowa)):
+    print(f"{i+1}.{slowa[i]}")
+print("b. wyświetli liczbę słów w pliku")
+ilosc = 0
+for i in range(len(slowa)):
+    ilosc+=1
+print(ilosc)
+print("c. wyświetli słowa zaczynające się na literę A")
+for i in range(len(slowa)):
+    if (slowa[i])[0] == "A":
+        print(slowa[i])
+print("d. wyświetli słowa kończące się na literę A")
+for i in range(len(slowa)):
+    if (slowa[i])[-1] == "A":
+        print(slowa[i])
+print("e. wyświetli słowa oraz liczbę liter, z których się składają")
+for slowo in slowa:
+    print(f"{len(slowo)-1} - slowo")
+print("f. *wyświetli najkrótsze oraz najdłuższe słowo w pliku oraz ich długość (jeżeli jest ich kilka wyświetlpierwsze z nich)")
+print("g. wyświetli słowa o długości 6")
+print("h. wyświetli słowa zawierające literę O oraz dla każdego z tych słów liczbę tych liter O")
+print("i. wyświetli ile razy w całym pliku występuje litera A")
+plik.close()
