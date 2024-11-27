d46c046551d48ee89ca48a22ada0cee4744ca559
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2024-11-27 15:07:08 +0100

    Update zadania.py

--- Informatyka/Znajdowanie_Podciągów/zadania.py
+++ Informatyka/Znajdowanie_Podciągów/zadania.py
@@ -24,3 +24,14 @@ for dl in range(1,n+1):
         for i in range(indeks_pocz, indeks_kon+1):
             print(ciag[i], end=" ")
         print()
+
+#zad.2
+plik = open('liczby.txt', 'r')
+ciag = list(map(int,plik.read().split()))
+plik.close()
+print(ciag)
+n = len(ciag)
+for index_pocz in range(n):
+    for dl in range(1, n+1-index_pocz):
+        print(*ciag[index_pocz:index_pocz+dl])
+    print()
