2ae2afeb8296d37198e0da2e45b8ab2096d45280
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2024-11-27 14:38:43 +0100

    Update zadania.py

--- Informatyka/Znajdowanie_Podciągów/zadania.py
+++ Informatyka/Znajdowanie_Podciągów/zadania.py
@@ -9,3 +9,18 @@ n = len(ciag)
 for dl in range(1,n+1):
     for indeks_pocz in range(n-dl+1):
         wyswietl_liczby_po_spacji(ciag[indeks_pocz:indeks_pocz+dl])
+
+#wersja bez slajsingu    
+def wyswietl_liczby_po_spacji(tablica):
+    print(' '.join(map(str,tablica)))
+plik = open('liczby.txt', 'r')
+ciag = list(map(int,plik.read().split()))
+plik.close()
+print(ciag)
+n = len(ciag)
+for dl in range(1,n+1):
+    for indeks_pocz in range(n-dl+1):
+        indeks_kon = indeks_pocz+dl-1
+        for i in range(indeks_pocz, indeks_kon+1):
+            print(ciag[i], end=" ")
+        print()
