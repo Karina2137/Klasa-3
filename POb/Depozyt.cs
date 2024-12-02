6d144a23865046be77f57cf6bb727aaf0350f6e6
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2024-12-02 15:10:24 +0100

    Create Depozyt.cs

---
+++ POb/Depozyt.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegat
+{
+	internal class Program
+	{
+		public delegate void NotificationHandler(string message);
+		public class EmailNotifier
+		{
+			public void SendEmail(string message)
+			{
+                Console.WriteLine($"Email wysłany: {message}");
+            }
+		}
+		public class SMSNotifier
+		{
+			public void SendSMS(string message)
+			{
+				Console.WriteLine($"SMS wysłany: {message}");
+			}
+		}
+		public class PushNotifier
+		{
+			public void SendPushNotifier(string message)
+			{
+                Console.WriteLine($"Powiadomienie PUSH wysłany: {message}");
+			}
+		}
+		public class NotificationManager
+		{
+			public NotificationHandler Notify;
+			public void AddNotificationMethod(NotificationHandler handler)
+			{
+				Notify += handler;
+			}
+			public void RemoveNotificationMethod(NotificationHandler handler)
+			{
+				Notify -= handler;
+			}
+			public void SendNotification(string message)
+			{
+				Notify?.Invoke(message);
+			}
+		}
+		static void Main(string[] args)
+		{
+			var emailNotifier = new EmailNotifier();
+			var smsNotifier = new SMSNotifier();
+			var pushNotifier = new PushNotifier();
+
+			var notificationManager = new NotificationManager();
+
+			notificationManager.AddNotificationMethod(emailNotifier.SendEmail);
+			notificationManager.AddNotificationMethod(smsNotifier.SendSMS);
+			notificationManager.AddNotificationMethod(pushNotifier.SendPushNotifier);
+			notificationManager.SendNotification("pierwsza wiadomość");
+            Console.WriteLine();
+            notificationManager.RemoveNotificationMethod(smsNotifier.SendSMS);
+			notificationManager.SendNotification("druga wiadomość");
+
+			Console.ReadKey();
+		}
+	}
+}
