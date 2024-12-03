0837e2a49d019eae9ec7960413f56e876569930d
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2024-12-03 11:17:43 +0100

    Update zadania.py

--- Informatyka/Znajdowanie_Podciągów/zadania.py
+++ Informatyka/Znajdowanie_Podciągów/zadania.py
@@ -35,3 +35,51 @@ for index_pocz in range(n):
     for dl in range(1, n+1-index_pocz):
         print(*ciag[index_pocz:index_pocz+dl])
     print()
+
+#zad.6
+ciag = list(map(int, input("Podaj ciąg liczb: ").split(' ')))
+def czy_rosnąca(tab):
+    n = len(tab)
+    for i in range(1, n):
+        if tab[i] <= tab[i-1]:
+            return ("nie")
+    return ("tak")
+print(czy_rosnąca(ciag))
+
+#zad.7
+def czy_malejąca(tab):
+    n = len(tab)
+    for i in range(1, n):
+        if tab[i] >= tab[i-1]:
+            return ("nie")
+    return ("tak")
+print(czy_malejąca(ciag))
+
+#zad.8
+def czy_nierosnący(tab):
+    n = len(tab)
+    for i in range(1, n):
+        if tab[i] > tab[i-1]:
+            return ("nie")
+    return ("tak")
+print(czy_nierosnący(ciag))
+
+#zad.9
+def czy_niemalejący(tab):
+    n = len(tab)
+    for i in range(1, n):
+        if tab[i] < tab[i-1]:
+            return ("nie")
+    return ("tak")
+print(czy_niemalejący(ciag))
+
+#zad.10
+def czy_równy(tab):
+    n = len(tab)
+    for i in range(1, n):
+        if tab[0] != tab[i]:
+            return ("nie")
+    return ("tak")
+print(czy_równy(ciag))
+
+#zad.11
