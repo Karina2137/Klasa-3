49e05cda2752ee4108f49ce0f882ff54e1af6a96
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2025-01-08 13:20:25 +0100

    Create TestowKalk.cs

---
+++ Desktopowe/TestowKalk.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public class Kalk
+    {
+        public int dodaj(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
+//klasa programu
