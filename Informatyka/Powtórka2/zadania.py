ed364fda31d5f05ffa890eaa4f99a4200f2d50c3
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2025-03-06 07:53:14 +0100

    Update zadania.py

--- Informatyka/Powtórka2/zadania.py
+++ Informatyka/Powtórka2/zadania.py
@@ -26,3 +26,31 @@ for i in range(1,n):
 if dl > dl_max:
     dl_max = dl
 print(dl_max)
+
+#zad.6
+# with open("ciag.txt", "r") as file:
+#     ciag = list(map(int, file.read().split()))
+# # #ciag = ciag[:5]
+# print(ciag)
+# n = len(ciag)
+# max_suma = ciag[0]
+# for pocz in range(n):
+#     for dl in range(1, n-pocz+1):
+#         suma = sum(ciag[pocz:pocz+dl])
+#         # #print(ciag[pocz:pocz + dl], suma)
+#         if suma>max_suma:
+#             max_suma=suma
+# print(max_suma)
+
+# #zad.5
+# plik = open("ciag.txt", "r")
+# ciag = list(map(int, plik.read().split()))
+# plik.close()
+# ciag = ciag[:5]
+# n=len(ciag)
+# max_suma=ciag[0]+ciag[1]+ciag[2]
+# for pocz in range(1,n-2):
+#     print(pocz)
+#     for i in range(3):
+#         print(ciag[pocz+i:3])
+#         suma =+ciag[pocz+i]
